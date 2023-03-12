a47b5dc2cf6b2c1e3541d08f67b78151c1ade949
Narawit <110091549+FaisWarZo@users.noreply.github.com>
2023-03-12 20:55:56 +0700

    fix: Edit_profile

--- pages/edit_profile/index.tsx
+++ pages/edit_profile/index.tsx
@@ -15,27 +15,17 @@ import { edit_Profile } from "@src/services/api/edit_profile"
 const EditUser = () => {
   const dispatch = useDispatch()
   const dataDetailUser = useSelector(dataProfile)
-  const [id, setId] = useState<string>()
-  const [username, setUsername] = useState<string>()
-  const [fname, setFname] = useState<string>()
-  const [lname, setLname] = useState<string>()
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm()
 
-  useEffect(() => {
-    if (dataDetailUser) {
-      setId(dataDetailUser.id)
-      setUsername(dataDetailUser.username)
-      setFname(dataDetailUser.first_name)
-      setLname(dataDetailUser.last_name)
-    }
-  }, [dataDetailUser])
-
   const onSubmit = async (_data) => {
-    const { status, data, message } = await edit_Profile(id, _data)
+    const { status, data, message } = await edit_Profile(
+      dataDetailUser?.id,
+      _data
+    )
     if (status && data) {
       await dispatch(
         edit_profile({
@@ -63,107 +53,109 @@ const EditUser = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mt-60 mb-[350px] flex-col justify-center">
           <h2 className="mb-9 text-center text-2xl text-white">EDIT PROFILE</h2>
-          <div className="grid place-content-center">
-            <div className="btn-input">
-              <div className="flex justify-between">
-                <h2>Username :</h2>
-                <input
-                  {...register("username", {
-                    required: "Please input your username.",
-                    minLength: {
-                      value: 4,
-                      message: "Username must have at least 4 characters"
-                    }
-                  })}
-                  type="text"
-                  id="username"
-                  value={username}
-                  defaultValue={username}
-                  placeholder="Username"
-                  className="ml-5 bg-colorinput-black"
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
-            </div>
-            {errors.username && (
-              <ErrorMessage
-                errors={errors}
-                name="username"
-                render={({ message }) => (
-                  <div className="mt-2 text-red-500">{message}</div>
+          {dataDetailUser ? (
+            <div>
+              <div className="grid place-content-center">
+                <div className="btn-input">
+                  <div className="flex justify-between">
+                    <h2>Username :</h2>
+                    <input
+                      {...register("username", {
+                        required: "Please input your username.",
+                        minLength: {
+                          value: 4,
+                          message: "Username must have at least 4 characters"
+                        }
+                      })}
+                      type="text"
+                      id="username"
+                      defaultValue={dataDetailUser?.username}
+                      placeholder="Username"
+                      className="ml-5 bg-colorinput-black"
+                    />
+                  </div>
+                </div>
+                {errors.username && (
+                  <ErrorMessage
+                    errors={errors}
+                    name="username"
+                    render={({ message }) => (
+                      <div className="mt-2 text-red-500">{message}</div>
+                    )}
+                  />
                 )}
-              />
-            )}
-            <div className="btn-input">
-              <div className="flex justify-between">
-                <h2>First name :</h2>
-                <input
-                  {...register("first_name", {
-                    required: "Please input your Firstname.",
-                    minLength: {
-                      value: 4,
-                      message: "Firstname must have at least 4 characters"
-                    }
-                  })}
-                  type="text"
-                  id="fname"
-                  value={fname}
-                  defaultValue={fname}
-                  placeholder="First name"
-                  className="ml-5 bg-colorinput-black"
-                  onChange={(e) => setFname(e.target.value)}
-                />
-              </div>
-            </div>
-            {errors.first_name && (
-              <ErrorMessage
-                errors={errors}
-                name="first_name"
-                render={({ message }) => (
-                  <div className="mt-2 text-red-500">{message}</div>
+                <div className="btn-input">
+                  <div className="flex justify-between">
+                    <h2>First name :</h2>
+                    <input
+                      {...register("first_name", {
+                        required: "Please input your Firstname.",
+                        minLength: {
+                          value: 4,
+                          message: "Firstname must have at least 4 characters"
+                        }
+                      })}
+                      type="text"
+                      id="fname"
+                      defaultValue={dataDetailUser?.first_name}
+                      placeholder="First name"
+                      className="ml-5 bg-colorinput-black"
+                    />
+                  </div>
+                </div>
+                {errors.first_name && (
+                  <ErrorMessage
+                    errors={errors}
+                    name="first_name"
+                    render={({ message }) => (
+                      <div className="mt-2 text-red-500">{message}</div>
+                    )}
+                  />
                 )}
-              />
-            )}
 
-            <div className="btn-input">
-              <div className="flex justify-between">
-                <h2>Last name :</h2>
-                <input
-                  {...register("last_name", {
-                    required: "Please input your Lastname.",
-                    minLength: {
-                      value: 4,
-                      message: "Lastname must have at least 4 characters"
-                    }
-                  })}
-                  type="text"
-                  id="lname"
-                  value={lname}
-                  defaultValue={lname}
-                  placeholder="Last name"
-                  className="ml-5 bg-colorinput-black"
-                  onChange={(e) => setLname(e.target.value)}
-                />
+                <div className="btn-input">
+                  <div className="flex justify-between">
+                    <h2>Last name :</h2>
+                    <input
+                      {...register("last_name", {
+                        required: "Please input your Lastname.",
+                        minLength: {
+                          value: 4,
+                          message: "Lastname must have at least 4 characters"
+                        }
+                      })}
+                      type="text"
+                      id="lname"
+                      defaultValue={dataDetailUser?.last_name}
+                      placeholder="Last name"
+                      className="ml-5 bg-colorinput-black"
+                    />
+                  </div>
+                </div>
+                {errors.last_name && (
+                  <ErrorMessage
+                    errors={errors}
+                    name="last_name"
+                    render={({ message }) => (
+                      <div className="mt-2 text-red-500">{message}</div>
+                    )}
+                  />
+                )}
+              </div>
+              <div className="container mt-12 flex min-w-full flex-col items-center">
+                <button
+                  className="button"
+                  type="submit"
+                >
+                  <h2>UPDATE</h2>
+                </button>
               </div>
             </div>
-            {errors.last_name && (
-              <ErrorMessage
-                errors={errors}
-                name="last_name"
-                render={({ message }) => (
-                  <div className="mt-2 text-red-500">{message}</div>
-                )}
-              />
-            )}
-          </div>
-          <div className="container mt-12 flex min-w-full flex-col items-center">
-            <button
-              className="button"
-              type="submit"
-            >
-              <h2>UPDATE</h2>
-            </button>
-          </div>
+          ) : (
+            <>
+              <h2 className="my-80 text-center text-white">Please login 👆</h2>
+            </>
+          )}
         </div>
       </form>
       <Footer />
